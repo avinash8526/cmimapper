e903e8b5739751ae90ab55ca9cf6b4c82572f3c8
Avinash Agrawal <avinash8526@gmail.com>
2013-07-15 21:09:23 +0530

    version 1.0

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="cmimapper",
+	  version="1.0",
+	  author="Avinash Agrawal",
+	  author_email="avinash8526@gmail.com",
+	  py_modules=["cmimapper"],
+	)
